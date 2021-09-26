701aae05a5d4928e0067317354e6878d0fb2a2a9
Frederic Marion-Poll <marion_poll@hotmail.com>
2021-09-26 09:53:23 +0200

    update maxwell file test

--- tests/HDF5.NET.Tests/ReadMaxWellFileTests.cs
+++ tests/HDF5.NET.Tests/ReadMaxWellFileTests.cs
@@ -11,20 +11,46 @@ namespace HDF5.NET.Tests
             int Electrode,
             int Channel,
             double XuM,
-            double YuM);
+            double YuM)
+        {
+            public int Electrode { get; } = Electrode;
+            public int Channel { get; } = Channel;
+            public double XuM { get; } = XuM;
+            public double YuM { get; } = YuM;
+        }
 
         [Fact]
         public void OpenAndReadH5MaxwellFileTest()
         {
-           // var localFilePath = "E:\\2021 MaxWell\\Trace_20210715_16_54_48_1mM(+++).raw.h5";          //HDD
-            var localFilePath = "C:\\Users\\fred\\Downloads\\Trace_20210715_16_54_48_1mM(+++).raw.h5"; //SSD
+            OpenTestFile();
+            var unused = ReadAll_OneElectrodeAsInt(GetChannelProperties(863));
+        }
+
+        [Fact]
+        public void OpenAndReadH5MaxwellFileWithThreadsTest()
+        {
+            OpenTestFile();
+            var unused = ReadAll_OneElectrodeAsIntParallel(GetChannelProperties(863));
+        }
+
+        private ElectrodeProperties GetChannelProperties(int channel)
+        {
+            return new ElectrodeProperties(0, channel, 0, 0);
+        }
+
+        private void OpenTestFile()
+        {
+            var localFilePath = GetFileNameHdd(true);
             if (!OpenReadMaxWellFile(localFilePath))
                 throw new Exception();
-
-            var electrodeProperties = new ElectrodeProperties(0, 683, 0, 0);
-            var unused = ReadAll_OneElectrodeAsInt(electrodeProperties);
-            //var result2 = ReadAll_OneElectrodeAsIntParallel(electrodeProperties);
         }
+
+        private string GetFileNameHdd(bool optionHdd)
+        {
+            return optionHdd ?  "E:\\2021 MaxWell\\Trace_20210715_16_54_48_1mM(+++).raw.h5"
+                                :"C:\\Users\\fred\\Downloads\\Trace_20210715_16_54_48_1mM(+++).raw.h5";
+        }
+
 
         public bool OpenReadMaxWellFile(string fileName)
         {
@@ -34,36 +60,42 @@ namespace HDF5.NET.Tests
 
         public ushort[] ReadAll_OneElectrodeAsInt(ElectrodeProperties electrodeProperties)
         {
-            H5Group group = Root.Group("/");
-            H5Dataset dataset = group.Dataset("sig");
-            int ndimensions = dataset.Space.Rank;
+            var h5Group = Root.Group("/");
+            var h5Dataset = h5Group.Dataset("sig");
+            int ndimensions = h5Dataset.Space.Rank;
             if (ndimensions != 2)
                 return null;
-            var nbdatapoints = dataset.Space.Dimensions[1];
-            return Read_OneElectrodeDataAsInt(dataset, electrodeProperties.Channel, 0, nbdatapoints - 1);
+            var nbdatapoints = h5Dataset.Space.Dimensions[1];
+            return Read_OneElectrodeDataAsInt(h5Dataset, electrodeProperties.Channel, 0, nbdatapoints - 1);
         }
 
         public ushort[] ReadAll_OneElectrodeAsIntParallel(ElectrodeProperties electrodeProperties)
         {
-            H5Group group = Root.Group("/");
-            H5Dataset dataset = group.Dataset("sig");
-            var nbdatapoints = dataset.Space.Dimensions[1];      // any size*
+            var h5Group = Root.Group("/");
+            var h5Dataset = h5Group.Dataset("sig");
+            var nbdatapoints = h5Dataset.Space.Dimensions[1];
             const ulong chunkSizePerChannel = 200;
             var result = new ushort[nbdatapoints];
             var nchunks = (long)(nbdatapoints / chunkSizePerChannel);
 
-            int ndimensions = dataset.Space.Rank;
+            int ndimensions = h5Dataset.Space.Rank;
             if (ndimensions != 2)
                 return null;
 
+            var fileName = GetFileNameHdd(true);
             Parallel.For(0, nchunks, i =>
             {
+                var lRoot = H5File.OpenRead(fileName);
+                var lgroup = lRoot.Group("/");
+                var ldataset = lgroup.Dataset("sig");
+
                 var istart = (ulong)i * chunkSizePerChannel;
                 var iend = istart + chunkSizePerChannel - 1;
                 if (iend > nbdatapoints)
                     iend = nbdatapoints - 1;
-                var chunkresult = Read_OneElectrodeDataAsInt(dataset, electrodeProperties.Channel, istart, iend);
+                var chunkresult = Read_OneElectrodeDataAsInt(ldataset, electrodeProperties.Channel, istart, iend);
                 Array.Copy(chunkresult, 0, result, (int)istart, (int)(iend - istart + 1));
+                lRoot.Dispose();
             });
 
             return result;
